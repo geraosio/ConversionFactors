705391c4178185b1cd348bf64f39d21d51154326
geraosio <geraosio@gmail.com>
2018-11-23 06:49:32 -0600

    Show really small and really big numbers in scientific notation with 4 significant digits
    Numbers smaller than 4 decimal places where rounded to 0 and big numbers made the label
    go off the screen or overlap with other content. The exponents in the scientific notation where shown
    as E12.
    
    * Add NumberFormatter for scientific notation with 4 significant digits
    * Replace the E, in the scientific notation, with "x 10^"
    * Show exponents after the caret (^) as superscripts with the func exponentize(numberAsString:)

--- Unit Conversion/Unit Conversion/Extensions/Float+Formatter.swift
+++ Unit Conversion/Unit Conversion/Extensions/Float+Formatter.swift
@@ -8,6 +8,48 @@
 
 import Foundation
 
+func exponentize(numberAsString: String) -> String {
+    
+    let superCodes = [
+        "\u{2070}", // 0
+        "\u{00B9}", // 1
+        "\u{00B2}", // 2
+        "\u{00B3}", // 3
+        "\u{2074}", // 4
+        "\u{2075}", // 5
+        "\u{2076}", // 6
+        "\u{2077}", // 7
+        "\u{2078}", // 8
+        "\u{2079}"  // 9
+    ]
+    
+    var exponentized = ""
+    var caretFound = false
+    
+    for digitAsChar in numberAsString {
+        
+        if digitAsChar == "^" {
+            caretFound = true
+        } else {
+            if caretFound {
+                let key = Int(String(digitAsChar)) ?? -1
+                if key > -1 && key < 10 {
+                    exponentized.append(superCodes[key])
+                } else if digitAsChar == "-" {
+                    exponentized.append("\u{207B}")
+                } else {
+                    caretFound = false
+                    exponentized.append(digitAsChar)
+                }
+            } else {
+                exponentized.append(digitAsChar)
+            }
+        }
+    }
+    
+    return exponentized
+}
+
 extension Float {
     static let fourFractionDigits: NumberFormatter = {
         let formatter = NumberFormatter()
@@ -17,7 +59,31 @@ extension Float {
         return formatter
     }()
     
+    static let scientificNotation: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .scientific
+        formatter.minimumSignificantDigits = 4
+        formatter.maximumSignificantDigits = 4
+        return formatter
+    }()
+    
     var formatted: String {
-        return Float.fourFractionDigits.string(for: self) ?? ""
+        
+        if self > 0.0001 && self < 1_000_000_000 {
+            // Return the float as string rounded to maximum 4 decimal digits with comma separation each 3 integers.
+            return Float.fourFractionDigits.string(for: self) ?? ""
+        } else {
+            // Get the float as string rounded to 4 significant digits in scientific notation (ex. "4.223E12")
+            let formattedNumber = Float.scientificNotation.string(for: self) ?? ""
+            var returnString = String()
+            
+            // Change the E sign to "x 10^"
+            for digit in formattedNumber {
+                digit == "E" ? returnString.append(" x 10^") : returnString.append(digit)
+            }
+            
+            // Return the string with the exponents as superscripts
+            return exponentize(numberAsString: returnString)
+        }
     }
 }
